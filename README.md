294a418108f4979ef6255da256ff3009285c0cc2
Samuel Corso <sam@usecanopy.com>
2022-04-08 18:03:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# business-insider
+
+## Install dependencies
+
+`$ npm install`
+
+## Start the project
+
+`$ npm start`
+
+This will open a command-line prompt to enter the URL of the article you want to save.
+
+The PDF will be saved in an `articles` directory.
---
+++ index.js
@@ -0,0 +1,92 @@
+import prompts from 'prompts';
+import axios from 'axios';
+import { JSDOM } from 'jsdom';
+import puppeteer from 'puppeteer'
+import fs from 'fs';
+import { promisify } from 'util';
+
+const exists = promisify(fs.exists);
+const mkdir = promisify(fs.mkdir);
+
+const acceptUrl = async () => {
+    const { url } = await prompts({
+        type: 'text',
+        name: 'url',
+        message: 'Enter an article URL to parse',
+        validate: value => new URL(value)
+    });
+    return url;
+}
+
+const fetchArticleTitleAndHtml = async ({ url }) => {
+    const { data: pageHtml } = await axios.get(url);
+    const { window: { document } } = new JSDOM(pageHtml);
+    const articleTitle = document.title;
+    const articleContainer = document.querySelector('#l-content article');
+    if (!articleContainer) {
+        throw new Error('Could not find the article on the page');
+    }
+    const aspectRatio = articleContainer.querySelector('.aspect-ratio');
+    aspectRatio.setAttribute('style', '');
+    const moreContentContainer = articleContainer.querySelector('.post-content-more');
+    const notificationPromptContainer = articleContainer.querySelector('.notification-prompt-wrapper');
+    const popularVideoContainer = articleContainer.querySelector('.popular-video');
+    const postContentContainer = articleContainer.querySelector('.post-content-category');
+    const postContentBottomContainer = articleContainer.querySelector('.post-content-bottom');
+    const elsToRemove = [
+        moreContentContainer,
+        notificationPromptContainer,
+        popularVideoContainer,
+        postContentContainer,
+        postContentBottomContainer
+    ].filter(Boolean);
+    for (const el of elsToRemove) {
+        el.remove();
+    }
+    const refreshPlayer = document.querySelector('.the-refresh-player-wrapper');
+    if (refreshPlayer) {
+        refreshPlayer.remove();
+    }
+    const articleHtml = articleContainer.innerHTML;
+    return { articleTitle, articleHtml };
+}
+
+const generateTitle = ({ articleTitle }) => {
+    const title = `${articleTitle.replace(/\s+/g, '-').toLowerCase()}.pdf`;
+    return title;
+}
+
+const generatePdf = async ({ articleTitle, articleHtml }) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(articleHtml);
+    if (!(await exists('articles'))) {
+        await mkdir('articles');
+    }
+    const path = `articles/${generateTitle({ articleTitle })}`;
+    await page.pdf({
+        path,
+        format: 'A4',
+        printBackground: true,
+        margin: {
+            top: '1cm',
+            right: '1cm',
+            bottom: '1cm',
+            left: '1cm'
+        }
+    });
+    await browser.close();
+    console.log(`Article saved to ${path}`);
+}
+
+const generateArticlePdf = async () => {
+    const url = await acceptUrl();
+    const { articleTitle, articleHtml } = await fetchArticleTitleAndHtml({ url });
+    await generatePdf({ articleTitle, articleHtml });
+}
+
+(async () => {
+    await generateArticlePdf();
+})();
+
+export default generateArticlePdf;
